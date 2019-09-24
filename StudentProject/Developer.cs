67c5af570f930da8ead2d3ce5dc5db28f6220153
Peter Johnson <peter.johnson@usli.com>
2019-09-24 10:16:38 -0400

    Add project files.

---
+++ StudentProject/Developer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject
+{
+    class Developer
+    {
+        public string firstName;
+        public string lastName;
+        public int phoneExtension;
+        public string gender;
+        public string fullName;
+
+        public Developer()
+        {
+            firstName = "Peter";
+            lastName = "Johnson";
+            phoneExtension = 856;
+            gender = "Male";
+            fullName = firstName + lastName;
+        }
+        public Developer(string fN, string lN, int phoneExt, string gen, string fullN)
+        {
+            fN = firstName;
+            lN = lastName;
+            phoneExt = phoneExtension;
+            gen = gender;
+            fullN = fullName;
+        }
+
+    }
+
+}
